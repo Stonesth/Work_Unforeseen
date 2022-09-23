d3ba9c71ca625156d68380dc85bc612aeca8d71d
Stonesth <pierre.thonon@gmail.com>
2022-09-23 11:32:11 +0200

    Final version

--- work_unforeseen.py
+++ work_unforeseen.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 from Tools import tools_v000 as tools
-from Topdesk import topdesk as t
+# from Topdesk import topdesk as t
 from Jira import jira as j
 from MyHours import myhours as m
+from ServiceNow import servicenow as sn
 import os
 from os.path import dirname
 import time
@@ -18,9 +19,14 @@ test = False # If False, start the clock else if True We are in test mode => not
 
 j.epic_link = tools.readProperty(propertiesFolder_path, 'Work_Unforeseen', 'epic_link=')
 j.save_path = tools.readProperty(propertiesFolder_path, 'Work_Unforeseen', 'save_path=')
-t.incidentNumber = tools.readProperty(propertiesFolder_path, 'Work_Unforeseen', 'incidentNumber=')
-t.sprint = tools.readProperty(propertiesFolder_path, 'Work_Unforeseen', 'sprint=')
-j.jira = tools.readProperty(propertiesFolder_path, 'Work_Unforeseen', 'jira=')
+sn.user_name = tools.readProperty(propertiesFolder_path, 'ServiceNow', 'user_name=')
+sn.incident_change_id = tools.readProperty(propertiesFolder_path, 'ServiceNow', 'incident_change_id=')
+
+userJira = tools.readProperty(propertiesFolder_path, 'JIRA', 'userJira=')
+passJira = tools.readProperty(propertiesFolder_path, 'JIRA', 'passJira=')
+teamName = tools.readProperty(propertiesFolder_path, 'JIRA', 'teamName=')
+reporterName = tools.readProperty(propertiesFolder_path, 'JIRA', 'reporterName=')
+assigneeName = tools.readProperty(propertiesFolder_path, 'JIRA', 'assigneeName=')
 
 # Open Browser
 tools.openBrowserChrome()
@@ -34,59 +40,55 @@ if test != True :
 else :
     print ("We are in test mode - no start new time")
 
-# TopDesk part
-t.connectViaLink()
-t.incidentTitle()
+# ServiceNow part
+sn.connectToServiceNow(sn.user_name)
+time.sleep(1)
+sn.connectToServiceNowIncidentChange(sn.incident_change_id)
+sn.collectData()
+
+print("Caller = ", sn.caller)
+print("incidentTitle = ", sn.incidentTitle )
+print("description_text = ", sn.description_text )
 
 # Jira part
 if test != True :
-    j.connectToJira(j.jira)
+    j.loginToJira('https://jira.atlassian.insim.biz/login.jsp?nosso', userJira, passJira)
 else :
-    j.connectToJiraTST(j.jira)
+    j.loginToJira('https://jira-test.atlassian.insim.biz/login.jsp?nosso', userJira, passJira)
+    
+# Create a new JIRA
+j.createJira("RUN - " + sn.incident_change_id + " - " + sn.incidentTitle, sn.description_text, sn.incident_change_id, teamName, reporterName, assigneeName)
 
+# Need to open the JIRA just created
+j.openJira("RUN - " + sn.incident_change_id + " - " + sn.incidentTitle)
+
+# Need to recovered the identifier of the jira
+tools.waitLoadingPageByID("key-val")
+j.jira = tools.driver.find_element_by_id("key-val").get_attribute('data-issue-key').encode('utf-8')
+
+print("JIRA = " + j.jira)
+
+# Need to recovered Information
 j.recoverJiraInformation()
 
-# # Place a comment into the JIRA and saved it
-# # click on the button footer-comment-button
-j.commentButton()
-
-# # Place a comment
-time.sleep(1)
-j.placeTheTextIntoComment(t.incidentNumber, t.incidentTitle)
-
-# # Add the comment
-j.addComment() 
-time.sleep(1)
-
 # Create folder link to this JIRA
-j.createFolderJira(j.jira + '/' + t.incidentNumber)
-if (t.incidentNumber.startswith("I")) :
-    j.createFileInto(j.jira, j.jiraTitle, t.incidentNumber + " - " + t.incidentTitle + "\n" + "https://nnbe.topdesk.net/tas/secure/incident?action=lookup&lookup=naam&lookupValue=" + t.incidentNumber + "\n", j.jira + '/' + t.incidentNumber, j.jira + '_' + t.incidentNumber + "_Comment_v001")
-else :
-    j.createFileInto(j.jira, j.jiraTitle, t.incidentNumber + " - " + t.incidentTitle + "\n" + "https://nnbe.topdesk.net/tas/secure/newchange?action=lookup&lookup=number&lookupValue=" + t.incidentNumber + "\n", j.jira + '/' + t.incidentNumber, j.jira + '_' + t.incidentNumber + "_Comment_v001")
+j.createFolderJira(j.jira)
+j.createFileInto(j.jira, j.jiraTitle, j.description_text, j.jira, j.jira + "_Comment_v001")
 
-# Start MyHours
-if test != True :
-    print ("Update the clock with the ticket")
-    m.connectToMyHours()
-    
-    time.sleep(2)
-    # Click on the current run
-    print ("Click on the current run")                     
-    timeStep1 = tools.driver.find_element_by_xpath('/html/body/div[1]/div/div/track-page/div/div[4]/div/div[2]')
-    timeStep1.click()    
-    time.sleep(2)
+# Update MyHours
+m.connectToMyHours()
 
-    m.modifyTrack(j.jira, j.jira + ' - ' + j.jiraTitle + ' - ' +  t.incidentNumber + ' - ' + t.incidentTitle, j.epic_link)
-else :
-    print ("We are in test mode - no start new time")
+# Click on the current run
+print ("Click on the current run")                     
+timeStep1 = tools.driver.find_element_by_xpath('/html/body/div[1]/div/div/track-page/div/div[5]/div/div[2]')
+timeStep1.click()    
+time.sleep(2)
+
+m.modifyTrack(j.jira, j.jira + ' - ' + j.jiraTitle, j.epic_link)
 
 # 
-tools.openFolder(j.save_path + j.jira + '\\' + t.incidentNumber)
-tools.openFile(j.save_path + j.jira + '/' + t.incidentNumber + '/' + j.jira + '_' +  t.incidentNumber + '_Comment_v001.txt')
-
-# Send mail to Menno Spelbos
-
+tools.openFolder(j.save_path + j.jira)
+tools.openFile(j.save_path + j.jira + '/' + j.jira + '_Comment_v001.txt')
 
 # Exit Chrome
 tools.driver.quit()
